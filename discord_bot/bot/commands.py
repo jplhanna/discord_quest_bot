2873cdc14eb02b6cb74b26b39ac3783b0dbc192f
jplhanna <jpl.hanna@gmail.com>
2022-02-03 00:00:51 -0500

    ping now requires admin permission to invoke

--- discord_bot/bot/commands.py
+++ discord_bot/bot/commands.py
@@ -1,9 +1,11 @@
 from discord.ext import commands
 from discord.ext.commands import Context
+from discord.ext.commands import has_permissions
 
 bot = commands.Bot(command_prefix='~')
 
 
 @bot.command(name='ping')
+@has_permissions(administrator=True)
 async def is_alive(ctx: Context) -> None:
     await ctx.send('pong')
