67d745fe049a300fe2ee20efcfd81eab8d1bca5d
jplhanna <jpl.hanna@gmail.com>
2022-04-20 02:35:33 -0400

    Updated test to use sentinel and ensure that function was called with expected id

--- bot/tests/test_controllers.py
+++ bot/tests/test_controllers.py
@@ -1,4 +1,5 @@
 from unittest.mock import MagicMock
+from unittest.mock import sentinel
 
 from pytest import mark
 
@@ -13,10 +14,12 @@ class TestCheckAndRegisterUser:
     @mark.parametrize("user_exists, expected_result", [(True, ALREADY_REGISTERED_MESSAGE), (False, NEW_USER_MESSAGE)])
     def test_account_already_exists(self, mock_container, user_exists, expected_result):
         # Arrange
+        ctx = MagicMock(author=MagicMock(id=sentinel.discord_id))
         mocked_user_service = MagicMock(get_user_by_discord_id=MagicMock(return_value=user_exists))
         mock_container.user_service.override(mocked_user_service)
         mock_container.wire(wire_to)
         # Act
-        res = check_and_register_user(MagicMock())
+        res = check_and_register_user(ctx)
         # Assert
+        mocked_user_service.get_user_by_discord_id.assert_called_with(sentinel.discord_id)
         assert res == expected_result
