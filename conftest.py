7ed042c45f97fb03c614b5b033aaa0286c971a1e
jplhanna <jpl.hanna@gmail.com>
2022-04-20 21:01:16 -0400

    Created set of tests to ensure query builder works

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
 from typing import Generator
+from unittest.mock import AsyncMock
 from unittest.mock import MagicMock
 
 from pytest import fixture
 
 from containers import Container
+from models import User
+from repositories import BaseRepository
 
 
 @fixture
@@ -12,3 +15,8 @@ def mock_container() -> Generator[Container, None, None]:
     mocked_container.init_resources()
     yield mocked_container
     mocked_container.unwire()
+
+
+@fixture()
+def mock_user_repository() -> BaseRepository[User]:
+    return BaseRepository(AsyncMock(), User)
---
+++ tests/test_repositories.py
@@ -0,0 +1,64 @@
+import pytest
+from pytest import mark
+
+from helpers.sqlalchemy_helpers import QueryArgs
+from models import User
+
+
+class TestUserRepository:
+    def test_query_base_case(self, mock_user_repository):
+        # Arrange
+        query_args = QueryArgs()
+        # Act & Assert
+        try:
+            mock_user_repository._query(query_args)
+        except:  # noqa
+            assert False, "Empty query failed to build"
+
+    @mark.parametrize(
+        "query_arg",
+        [
+            {"filter_list": []},
+            {"filter_dict": {}},
+            {"eager_options": []},
+            {"order_by_list": []},
+            {"join_list": []},
+            {"group_by_list": []},
+            {"having_list": []},
+        ],
+    )
+    def test_query_builder_with_empty_values(self, mock_user_repository, query_arg):
+        # Arrange
+        query_args = QueryArgs(**query_arg)
+        # Act & Assert
+        try:
+            mock_user_repository._query(query_args)
+        except:  # noqa
+            assert False, f"Query with empty args failed to build: {query_args}"
+
+    # TECH DEBT Join and eager options are untested because currently there are no tables to join onto
+    @mark.parametrize(
+        "query_args",
+        [
+            {"filter_list": [User.id == 1]},
+            {"filter_dict": dict(id=1)},
+            {"order_by_list": [User.id]},
+            {"distinct_on_list": []},
+            {"distinct_on_list": [User.id]},
+            {"group_by_list": [User.id]},
+            {"grou_by_list": [User.id], "having_list": [User.id == 1]},
+        ],
+    )
+    def test_query_builder_with_non_empty_values(self, mock_user_repository, query_args):
+        # Arrange
+        query_args = QueryArgs(**query_args)
+        # Act & Assert
+        try:
+            mock_user_repository._query(query_args)
+        except:  # noqa
+            assert False, f"Query failed to build with non emtpy values {query_args}"
+
+    def test_query_with_having_and_no_grouping_fails(self, mock_user_repository):
+        # Arrange & Act & Assert
+        with pytest.raises(Warning, match="Defining query with having clause but no group by clause"):
+            QueryArgs(having_list=[User.id == 1])
