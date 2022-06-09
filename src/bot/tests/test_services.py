e7bd01b8b762cc0438bef47ce277e9f0cac489bd
jplhanna <jpl.hanna@gmail.com>
2022-06-09 02:04:18 -0400

    Cleaned up service test

--- src/bot/tests/test_services.py
+++ src/bot/tests/test_services.py
@@ -15,7 +15,11 @@ class TestQuestService:
     )
     async def test_accept_quest_if_available(self, mocked_user, mock_container, quest_exists, result):
         # Arrange
-        mock_quest_repository = AsyncMagicMock(get_first=CoroutineMock(return_value=quest_exists))
+        session_factory = AsyncMagicMock()
+        session_factory.return_value.__aenter__.return_value.commit = CoroutineMock()
+        mock_quest_repository = AsyncMagicMock(
+            get_first=CoroutineMock(return_value=quest_exists), session_factory=session_factory
+        )
         quest_service = QuestService(repository=mock_quest_repository)
         # Act
         res = await quest_service.accept_quest_if_available(mocked_user, sentinel.quest_name)
