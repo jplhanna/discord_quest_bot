e295905682942ffb5e75dc347da606097cad27b6
jplhanna <jpl.hanna@gmail.com>
2022-08-08 21:44:01 -0400

    Writing paramerterized test for checking that length is calculated correctly

--- src/helpers/test_message_helpers.py
+++ src/helpers/test_message_helpers.py
@@ -1,3 +1,5 @@
+from pytest import mark
+
 from src.helpers.message_helpers import format_quest_board
 from src.models import Quest
 
@@ -23,3 +25,11 @@ class TestFormatQuestBoard:
             "======================\n"
             "```"
         )
+
+    @mark.parametrize("name, experience, line_length", [("", 0, 18), ("abcde", 50000, 20)])
+    def test_multi_case(self, name, experience, line_length):
+        quests = [Quest(name=name, experience=experience)]
+        board = format_quest_board(quests)
+        lines = board.split("\n")
+        test_lines = [line for line in lines if line != "```"]
+        assert all([len(line) == line_length for line in test_lines]), test_lines
