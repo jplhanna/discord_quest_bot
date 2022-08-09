6953177478326ac35d82ebde270aa0a6af05bdd2
jplhanna <jpl.hanna@gmail.com>
2022-08-08 20:27:32 -0400

    Created helper function to format quest board message, still needs work because discord characters are not all the same length

---
+++ src/helpers/test_message_helpers.py
@@ -0,0 +1,23 @@
+from src.helpers.message_helpers import format_quest_board
+from src.models import Quest
+
+
+class TestFormatQuestBoard:
+    def test_base_case(self):
+        # Act
+        board = format_quest_board([])
+        # Assert
+        assert board == "No available quests"
+
+    def test_single_case(self):
+        # Arrange
+        quest = Quest(name="Test Quest", experience=50)
+        # Act
+        board = format_quest_board([quest])
+        # Assert
+        assert (
+            board == "====================\n"
+            "=  **Quests      XP**  =\n"
+            "=  **Test Quest  50**  =\n"
+            "===================="
+        )
