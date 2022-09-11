1770469c99461acfc44ae3ab28c457efcc148c32
jplhanna <jpl.hanna@gmail.com>
2022-09-11 16:03:13 -0400

    Cleaning up pytest flake8 error

--- src/conftest.py
+++ src/conftest.py
@@ -7,8 +7,8 @@ from unittest.mock import MagicMock
 from unittest.mock import Mock
 from unittest.mock import sentinel
 
+import pytest
 from asynctest import MagicMock as AsyncMagicMock
-from pytest import fixture
 from sqlalchemy import inspect
 
 from src.containers import Container
@@ -21,7 +21,7 @@ from src.test_config import test_config_dict
 base_mock_container = Container(discord_logging=MagicMock())
 
 
-@fixture
+@pytest.fixture()
 def mock_container() -> Generator[Container, None, None]:
     mocked_container = copy(base_mock_container)
     mocked_container.db_client = MagicMock()
@@ -30,24 +30,24 @@ def mock_container() -> Generator[Container, None, None]:
     mocked_container.unwire()
 
 
-@fixture
+@pytest.fixture()
 def mocked_user() -> User:
     user = Mock(spec=User, discord_id=sentinel.discord_id)
     return user
 
 
-@fixture()
+@pytest.fixture()
 def mock_user_repository() -> BaseRepository[User]:
     return BaseRepository(AsyncMagicMock(), User)
 
 
-@fixture
+@pytest.fixture()
 def mock_user_with_db_repository(mock_user_repository, db_session):
     mock_user_repository.session_factory.return_value = db_session
     return mock_user_repository
 
 
-@fixture(scope="session")
+@pytest.fixture(scope="session")
 def container_for_testing() -> Generator[Container, None, None]:
     testing_container = copy(base_mock_container)
     testing_container.config.from_dict(test_config_dict)  # type: ignore[arg-type]
@@ -56,24 +56,24 @@ def container_for_testing() -> Generator[Container, None, None]:
     testing_container.unwire()
 
 
-@fixture(scope="session")
+@pytest.fixture(scope="session")
 def event_loop() -> Generator[AbstractEventLoop, None, None]:
     loop = new_event_loop()
     yield loop
     loop.close()
 
 
-@fixture(scope="session")
-def _database_url() -> str:
+@pytest.fixture(scope="session")
+def _database_url() -> str:  # noqa: PT005
     return TEST_ASYNC_DATABASE_URI
 
 
-@fixture(scope="session")
+@pytest.fixture(scope="session")
 def init_database() -> Callable:
     return BaseModel.metadata.create_all
 
 
-@fixture()
+@pytest.fixture()
 async def db_user(db_session):
     user = User(discord_id=1234567890)
     db_session.add(user)
