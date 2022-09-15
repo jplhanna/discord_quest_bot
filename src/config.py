fefaa055c8ceb2425b5f47bdc2540ecd0a4a145d
Jon-Pierre Hanna <14983511+jplhanna@users.noreply.github.com>
2022-09-14 20:28:48 -0400

    Fix discord logging (#21)
    * Fixing issued with logging created after the upgrade to discord.py 2.0.
    
    * Fixed logging to use a single logging dictionary
    
    * Removing unneeded logging setup class
    
    * Deleting logging.ini since it is being setup by a cfg dictionary

--- src/config.py
+++ src/config.py
@@ -1,11 +1,17 @@
 import os
 from copy import copy
+from logging import DEBUG
+from logging import INFO
+from logging import NOTSET
 
 from furl import furl
 
 from src.typeshed import ConfigDict
 from src.typeshed import DBConfigDict
 from src.typeshed import DiscordConfigDict
+from src.typeshed import FormatterDict
+from src.typeshed import LoggerDict
+from src.typeshed import LoggerItemDict
 
 ASYNC_PSQL_SCHEME = "postgresql+asyncpg"
 
@@ -39,7 +45,38 @@ config_dict = ConfigDict(
     db=DBConfigDict(database_uri=DATABASE_URI, async_database_uri=ASYNC_DATABASE_URI),
     discord=DiscordConfigDict(
         account_token=DISCORD_ACCOUNT_TOKEN,
-        log_filename=DISCORD_LOG_FILENAME,
-        log_level="DEBUG",
+    ),
+    logger=LoggerDict(
+        version=1,
+        formatters={
+            "base_formatter": FormatterDict(format="[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s"),
+            "simple_formatter": FormatterDict(format="[%(asctime)s] [%(levelname)s]: %(message)s"),
+        },
+        handlers={
+            "file_handler": {
+                "class": "logging.FileHandler",
+                "formatter": "base_formatter",
+                "filename": DISCORD_LOG_FILENAME,
+                "encoding": "utf-8",
+                "mode": "w",
+            },
+            "error_handler": {
+                "class": "logging.StreamHandler",
+                "level": DEBUG,
+                "formatter": "base_formatter",
+                "stream": "ext://sys.stderr",
+            },
+            "basic_handler": {
+                "class": "logging.StreamHandler",
+                "level": INFO,
+                "formatter": "simple_formatter",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        loggers={
+            "discord": LoggerItemDict(level=DEBUG, handlers=["file_handler"]),
+            "discord.http": LoggerItemDict(level=INFO, handlers=["basic_handler"]),
+        },
+        root=LoggerItemDict(handlers=["error_handler"], level=NOTSET),
     ),
 )
