96a46c45fef7a1b4f6800721017fd099ed617fd6
jplhanna <jpl.hanna@gmail.com>
2022-09-15 00:17:14 -0400

    Add service functions for completing a quest

--- src/services.py
+++ src/services.py
@@ -1,5 +1,6 @@
 from logging import Logger
 from logging import getLogger
+from typing import Coroutine
 from typing import Optional
 
 from sqlalchemy.orm import selectinload
@@ -9,6 +10,7 @@ from src.constants import QUEST_ALREADY_ACCEPTED
 from src.constants import QUEST_DOES_NOT_EXIST
 from src.helpers.sqlalchemy_helpers import QueryArgs
 from src.helpers.sqlalchemy_helpers import case_insensitive_str_compare
+from src.models import ExperienceTransaction
 from src.models import Quest
 from src.models import User
 from src.repositories import BaseRepository
@@ -38,13 +40,16 @@ class UserService(BaseService):
 class QuestService(BaseService):
     _repository: BaseRepository[Quest]
 
-    async def accept_quest_if_available(self, user: User, quest_name: str) -> str:
-        quest = await self._repository.get_first(
+    def _get_quest_by_name(self, quest_name: str) -> Coroutine[None, None, Optional[Quest]]:
+        return self._repository.get_first(
             QueryArgs(
                 filter_list=[case_insensitive_str_compare(Quest.name, quest_name)],
                 eager_options=[selectinload(Quest.users)],
             )
         )
+
+    async def accept_quest_if_available(self, user: User, quest_name: str) -> str:
+        quest = await self._get_quest_by_name(quest_name)
         if not quest:
             return QUEST_DOES_NOT_EXIST
 
@@ -55,6 +60,23 @@ class QuestService(BaseService):
         await self._repository.session.commit()
         return GOOD_LUCK_ADVENTURER.format(quest_name)
 
+    async def complete_quest_if_available(self, user: User, quest_name: str) -> str:
+        quest = await self._get_quest_by_name(quest_name)
+        if not quest:
+            return QUEST_DOES_NOT_EXIST
+
+        if user not in quest.users:
+            return "You have not accepted this quest."
+        return ""
+
     async def get_all_quests(self) -> list[Quest]:
         quests = await self._repository.get_all()
         return quests
+
+
+class ExperienceTransactionService(BaseService):
+    _repository: BaseRepository[ExperienceTransaction]
+
+    async def earn_xp_for_quest(self, user: User, quest: Quest) -> ExperienceTransaction:
+        xp_transaction = await self._repository.create(user=user, quest=quest)
+        return xp_transaction
