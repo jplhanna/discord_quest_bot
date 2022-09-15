6061eb7ffa726251e85c197ca70f3553ecb5b984
jplhanna <jpl.hanna@gmail.com>
2022-09-14 23:51:46 -0400

    Added Doc string to classes, also created table to track a user earning experience

--- src/models.py
+++ src/models.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from sqlalchemy import BigInteger
 from sqlalchemy import Column
 from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
@@ -33,6 +34,18 @@ class CoreModelMixin(metaclass=TableMeta):
 @mapper_registry.mapped
 @dataclass
 class User(CoreModelMixin):
+    """
+    Representation of a registered discord user
+
+    ...
+    Attributes
+    __________
+    discord_id: int
+        The discord side id
+    quests: list[Quest]
+        List of quests that a user has accepted
+    """
+
     # Columns
     discord_id: int = field(metadata={"sa": Column(BigInteger, unique=True)})
 
@@ -42,19 +55,60 @@ class User(CoreModelMixin):
         repr=False,
         metadata={"sa": relationship("Quest", secondary="user_quest", back_populates="users")},
     )
+    experience: list["ExperienceTransaction"] = field(
+        default_factory=list, repr=False, metadata={"sa": relationship("ExperienceTransaction", back_populates="user")}
+    )
 
 
 @mapper_registry.mapped
 @dataclass
 class Quest(CoreModelMixin):
+    """
+    A representation of an event(quest) a user can take part in
+
+    Attributes
+    __________
+    name: str
+        Name of the quest
+    experience: int
+        How much experience a user will earn upon completion of a quest
+    users: list[User]
+        List of users who have accepted this quest
+    """
+
     # Columns
     name: str = field(metadata={"sa": Column(String, nullable=False)})
     experience: int = field(metadata={"sa": Column(Integer, nullable=False)})
 
     # Relationships
     users: list[User] = field(
-        default_factory=list, metadata={"sa": relationship("User", secondary="user_quest", back_populates="quests")}
+        default_factory=list, metadata={"sa": relationship(User, secondary="user_quest", back_populates="quests")}
     )
 
 
 user_quest = many_to_many_table("User", "Quest")
+
+
+@mapper_registry.mapped
+@dataclass
+class ExperienceTransaction(CoreModelMixin):
+    """
+    Representation of an instance of a user earning experience
+
+    Attributes
+    __________
+    user_id: int
+        ID for user who earned this experience
+    quest_id: int
+        ID for quest that the user earned this experience from
+    user: User
+    quest: Quest
+    """
+
+    # Columns
+    user_id: int = field(init=False, metadata={"sa": Column(ForeignKey("user.id"), nullable=False)})
+    quest_id: int = field(init=False, metadata={"sa": Column(ForeignKey("quest.id"), nullable=False)})
+
+    # Relationship
+    user: User = field(metadata={"sa": relationship(User, back_populates="experience")})
+    quest: User = field(metadata={"sa": relationship(Quest)})
