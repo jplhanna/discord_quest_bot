317f7071cbd8aa9d7462a573a4c58757427bd6f5
jplhanna <jpl.hanna@gmail.com>
2022-09-14 20:17:58 -0400

    Fix mock container

--- src/conftest.py
+++ src/conftest.py
@@ -18,7 +18,7 @@ from src.repositories import BaseRepository
 from src.test_config import TEST_ASYNC_DATABASE_URI
 from src.test_config import test_config_dict
 
-base_mock_container = Container(discord_logging=MagicMock())
+base_mock_container = Container()
 
 
 @pytest.fixture()
