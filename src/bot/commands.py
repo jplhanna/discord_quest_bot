5e9445d9b137fc03cea447e89ad4915fce95b6a7
Jon-Pierre Hanna <14983511+jplhanna@users.noreply.github.com>
2023-07-27 21:27:16 -0400

    Update packages (#26)

--- src/bot/commands.py
+++ src/bot/commands.py
@@ -1,3 +1,5 @@
+from typing import cast
+
 from discord import Intents
 from discord.ext.commands import Bot
 from discord.ext.commands import Context
@@ -9,27 +11,31 @@ from src.bot.controllers import add_quest_to_user
 from src.bot.controllers import check_and_register_user
 from src.bot.controllers import complete_quest_for_user
 from src.bot.controllers import get_quest_list_text
+from src.bot.typeshed import CommandRegisterType
 from src.config import DISCORD_OWNER_ID
 
 default_intent = Intents.default()
 default_intent.message_content = True  # pylint: disable=E0237
 bot = Bot(command_prefix="~", intents=default_intent, owner_id=DISCORD_OWNER_ID)
 
+bot_hybrid_command_register = cast(CommandRegisterType, bot.hybrid_command)
+bot_command_register = cast(CommandRegisterType, bot.command)
 
-@bot.command(name="ping", help="Check that server is live")  # type: ignore[arg-type]
+
+@bot_command_register(name="ping", help="Check that server is live")
 @has_permissions(administrator=True)
 @is_owner()
 async def is_alive(ctx: Context) -> None:
     await ctx.send("pong")
 
 
-@bot.hybrid_command(name="register", help="Register user with the Quest Server")
+@bot_hybrid_command_register(name="register", help="Register user with the Quest Server")
 async def register_user(ctx: Context) -> None:
     res = await check_and_register_user(ctx)
     await ctx.send(res)
 
 
-@bot.hybrid_command(name="accept-quest", help="Accept a quest by name")
+@bot_hybrid_command_register(name="accept-quest", help="Accept a quest by name")
 async def accept_quest(ctx: Context, *, quest_name: str) -> None:
     res = await add_quest_to_user(ctx, quest_name)
     await ctx.send(res)
@@ -41,7 +47,7 @@ async def accept_quest_error(ctx: Context, error: Exception) -> None:
         await ctx.send("Must provide a quest to join")
 
 
-@bot.hybrid_command(
+@bot_hybrid_command_register(
     name="quests", aliases=["quest-board", "board"], help="Return a list of all currently available quests"
 )
 async def get_quest_board(ctx: Context) -> None:
@@ -49,15 +55,13 @@ async def get_quest_board(ctx: Context) -> None:
     await ctx.send(res)
 
 
-@bot.hybrid_command(name="complete-quest", help="Complete an available quest")
+@bot_hybrid_command_register(name="complete-quest", help="Complete an available quest")
 async def completed_quest(ctx: Context, *, quest_name: str) -> None:
     res = await complete_quest_for_user(ctx, quest_name)
     await ctx.send(res)
 
 
-@bot.command(  # type: ignore[arg-type]
-    name="sync", help="Sync bot with discord server in order to update available slash commands"
-)
+@bot_command_register(name="sync", help="Sync bot with discord server in order to update available slash commands")
 @is_owner()
 async def sync_bot_commands(_: Context) -> None:
     await bot.tree.sync()
