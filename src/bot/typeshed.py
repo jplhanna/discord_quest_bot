2dfb52e5fa7d95a9f560b651491918a132c47db2
jplhanna <jpl.hanna@gmail.com>
2023-07-28 00:25:35 -0400

    Fix docker build issue with not having access to mypy breaking starting the app

--- src/bot/typeshed.py
+++ src/bot/typeshed.py
@@ -2,19 +2,24 @@ from typing import Callable
 from typing import Concatenate
 from typing import Coroutine
 from typing import ParamSpec
+from typing import TYPE_CHECKING
 
 from discord.ext.commands import Context
 from discord.ext.commands import HybridCommand
-from mypy_extensions import DefaultNamedArg
-from mypy_extensions import NamedArg
 
-P = ParamSpec("P")
-CommandFunc = (
-    Callable[Concatenate[Context, P], Coroutine]
-    | Callable[[Context], Coroutine]
-    | Callable[[Context, NamedArg(str, "quest_name")], Coroutine]  # noqa: F821
-)
-CommandRegisterType = Callable[
-    [NamedArg(str, "name"), NamedArg(str, "help"), DefaultNamedArg(list[str], "aliases")],  # noqa: F821
-    Callable[[CommandFunc], HybridCommand],
-]
+if TYPE_CHECKING:
+    from mypy_extensions import DefaultNamedArg
+    from mypy_extensions import NamedArg
+
+    P = ParamSpec("P")
+    CommandFunc = (
+        Callable[Concatenate[Context, P], Coroutine]
+        | Callable[[Context], Coroutine]
+        | Callable[[Context, NamedArg(str, "quest_name")], Coroutine]  # noqa: F821
+    )
+    CommandRegisterType = Callable[
+        [NamedArg(str, "name"), NamedArg(str, "help"), DefaultNamedArg(list[str], "aliases")],  # noqa: F821
+        Callable[[CommandFunc], HybridCommand],
+    ]
+else:
+    CommandRegisterType = Callable
