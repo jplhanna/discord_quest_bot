659d692c17f16986754d16a8cf1cf3e5c55c02fe
JP Hanna <jpl.hanna@gmail.com>
2023-08-27 12:11:14 -0400

    Update ignores to include easy local files, also update user repo mock to properly mock out

--- src/conftest.py
+++ src/conftest.py
@@ -44,7 +44,7 @@ def mocked_ctx() -> MagicMock:
 
 @pytest.fixture()
 def mock_user_repository() -> BaseRepository[User]:
-    return BaseRepository(AsyncMock(), User)
+    return BaseRepository(MagicMock(session=AsyncMock()), User)
 
 
 @pytest.fixture()
