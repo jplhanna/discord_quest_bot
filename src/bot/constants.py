72582791d7fefe5f5526ea616492492f5127c2b4
jplhanna <14983511+jplhanna@users.noreply.github.com>
2026-04-10 19:44:13 -0400

    [DQB-43] Fix final definitions and consolidate date docker compose commands

--- src/bot/constants.py
+++ src/bot/constants.py
@@ -1,10 +1,10 @@
 from typing import Final
 
-NEW_USER_MESSAGE: Final = "You have been registered, prepare for adventure!"
-ALREADY_REGISTERED_MESSAGE: Final = "You have already registered"
-REGISTER_FIRST_MESSAGE: Final = "Please register first"
-NO_MENU_THIS_WEEK_MESSAGE: Final = "No menu has been created for this week."
-SERVER_ONLY_BAD_REQUEST_MESSAGE: Final = "Bad request, this feature is only available in servers."
-NO_MENU_ITEMS_FOR_CHOSEN_DAY_MESSAGE: Final = "No items to select for the day you asked."
-NO_SUCH_THEME_EXISTS: Final = "Unable to fulfill request, no such theme exists"
-STORY_HAS_BEEN_RECORDED: Final = "Tale has been added to the book of stories"
+NEW_USER_MESSAGE: Final[str] = "You have been registered, prepare for adventure!"
+ALREADY_REGISTERED_MESSAGE: Final[str] = "You have already registered"
+REGISTER_FIRST_MESSAGE: Final[str] = "Please register first"
+NO_MENU_THIS_WEEK_MESSAGE: Final[str] = "No menu has been created for this week."
+SERVER_ONLY_BAD_REQUEST_MESSAGE: Final[str] = "Bad request, this feature is only available in servers."
+NO_MENU_ITEMS_FOR_CHOSEN_DAY_MESSAGE: Final[str] = "No items to select for the day you asked."
+NO_SUCH_THEME_EXISTS: Final[str] = "Unable to fulfill request, no such theme exists"
+STORY_HAS_BEEN_RECORDED: Final[str] = "Tale has been added to the book of stories"
--- src/helpers/constants.py
+++ src/helpers/constants.py
@@ -1,11 +1,11 @@
 from typing import Final
 
-QUEST_COLUMN_NAME: Final = "Quests"
-EXPERIENCE_COLUMN_NAME: Final = "XP"
-BOX_VERTICAL_CHAR: Final = "="
-BOX_SIDE_CHAR: Final = "||"
-BLOCK_PRE_TEXT: Final = BOX_SIDE_CHAR + "  "
-BLOCK_POST_TEXT: Final = "  " + BOX_SIDE_CHAR
-CODE_BLOCK: Final = "```"
-WRAPPER_TEXT_LEN: Final = len(BLOCK_POST_TEXT) + len(BLOCK_PRE_TEXT)
-MINIMUM_SPACING: Final = 2
+QUEST_COLUMN_NAME: Final[str] = "Quests"
+EXPERIENCE_COLUMN_NAME: Final[str] = "XP"
+BOX_VERTICAL_CHAR: Final[str] = "="
+BOX_SIDE_CHAR: Final[str] = "||"
+BLOCK_PRE_TEXT: Final[str] = BOX_SIDE_CHAR + "  "
+BLOCK_POST_TEXT: Final[str] = "  " + BOX_SIDE_CHAR
+CODE_BLOCK: Final[str] = "```"
+WRAPPER_TEXT_LEN: Final[int] = len(BLOCK_POST_TEXT) + len(BLOCK_PRE_TEXT)
+MINIMUM_SPACING: Final[int] = 2
